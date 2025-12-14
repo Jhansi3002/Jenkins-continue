ebe149be55774cf41e4a82e2195ab512074b26b8
Jhansi3002 <jhansivjainraksha@gmail.com>
2025-12-14 16:30:19 +0530

    Initial Jenkins Docker project

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                sh 'docker build -t jenkins-python-app .'
+            }
+        }
+
+        stage('Stop Old Container') {
+            steps {
+                sh '''
+                docker stop prod-python-app || true
+                docker rm prod-python-app || true
+                '''
+            }
+        }
+
+        stage('Deploy Application (CD)') {
+            steps {
+                sh 'docker-compose up -d'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo "✅ Application deployed successfully!"
+        }
+        failure {
+            echo "❌ Deployment failed!"
+        }
+    }
+}
